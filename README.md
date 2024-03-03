f448aa8fcf32ee729cedec5925229440f4976436
jinyangTF <jinyang@tinyfish.io>
2024-03-03 08:58:47 -0800

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # HRDHack
+
+## Start backend server
+
+`cd backend`
+`npm i`
+`npm run dev`
+
+## Start frontend applicaiton
+
+`cd mobileFrontEnd`
+`yarn install`
+`npx expo start`
+
+You can access the interface either on http://localhost:8081 or via the Expo Go app on your phone 
--- mobileFrontEnd/api/getDiscussion.ts
+++ mobileFrontEnd/api/getDiscussion.ts
@@ -7,7 +7,6 @@ export async function getDiscussion(groupName: string) {
         const response = await axios.get(
             `${API_URL}/api/get_discussion/${groupName}`
         );
-        console.log(response.data.comments)
         return response.data;
     } catch (error) {
         console.error(error);
