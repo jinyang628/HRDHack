5edf92a273019365b2bd10c68734c1a26825dee4
jinyangTF <jinyang@tinyfish.io>
2024-03-02 16:13:44 -0800

    Set up frontend

---
+++ frontend/src/components/button.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+function Button(props) {
+  return (
+    <div className="justify-center items-start py-2.5 pr-16 pl-2.5 text-xs whitespace-nowrap rounded border border-solid border-zinc-500 max-w-[345px] text-zinc-500">
+      Enter Input
+    </div>
+  );
+}
